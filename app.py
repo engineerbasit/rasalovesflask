121b51f6d901451341148412b52c797305912c56
Abdul Basit <101786218+engineerbasit@users.noreply.github.com>
2022-03-29 14:29:11 +0500

    port updated from 5000 to 5005

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def extract():
   result=resp
   return render_template('index.html', result=result,text=text)
 if __name__ == "__main__":
-  p = int(os.environ.get("PORT", 5000))
+  p = int(os.environ.get("PORT", 5005))
   app.run(debug=True, port=p, host='https://rasaflask.herokuapp.com/')
