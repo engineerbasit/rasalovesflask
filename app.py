8d3a33169d33e40e253d20631175e326b8413f10
Abdul Basit <101786218+engineerbasit@users.noreply.github.com>
2022-03-29 14:24:48 +0500

    host updated

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def extract():
   return render_template('index.html', result=result,text=text)
 if __name__ == "__main__":
   p = int(os.environ.get("PORT", 5000))
-  app.run(debug=True, port=p, host='0.0.0.0')
+  app.run(debug=True, port=p, host='https://rasaflask.herokuapp.com/')
